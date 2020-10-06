97ea9b64842fa6eb8d6e1f1ad0fe5ea4ca35c395
Chris Birrell <vaiezmedia@gmail.com>
2020-10-06 15:59:15 +0100

    Feat: Added date&time picker plugin for "book now" modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,13 @@ $(function(){
         $('.nav-list').toggleClass('open');
     });
 });
+
+$('#datetimepicker1').datetimepicker();
+
+$('#btnViewDate').click(function() {
+  $('#datetimepicker1').datetimepicker('viewDate', moment('11/21/2018', 'MM/DD/YYYY') );
+});
+
+$('#btnDate').click(function() {
+  $('#datetimepicker1').datetimepicker('date', moment('11/21/2018', 'MM/DD/YYYY') );
+});
