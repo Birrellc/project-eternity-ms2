fcf052064c0d55ccacdb062ca4e69fc783844e4b
Chris Birrell <vaiezmedia@gmail.com>
2020-10-19 16:09:26 +0100

    Feat: added localstorage dark mode

---
+++ assets/js/color-scheme.js
@@ -0,0 +1,32 @@
+$(".dark-mode-btn").on('click', function() {
+    if ($("body").hasClass("dark-mode")) {
+        $("body").removeClass("dark-mode");
+        $("#hero").removeClass("hero-dark");
+        $("#divider-1").removeClass("divider-bg-1-dark");
+        $("#divider-2").removeClass("divider-bg-2-dark");
+        $(".card").removeClass("card-dark");
+        $("#contact").removeClass("contact-section-dark")
+        localStorage.removeItem("darkMode")}
+    else {
+        $("body").addClass("dark-mode");
+        $("#hero").addClass("hero-dark");
+        $("#divider-1").addClass("divider-bg-1-dark");
+        $("#divider-2").addClass("divider-bg-2-dark");
+        $(".card").addClass("card-dark");
+        $("#contact").addClass("contact-section-dark")
+        localStorage.setItem("darkMode", true);
+    }
+});
+
+onload = function() {
+    if(localStorage.getItem("darkMode")) {
+        localStorage.getItem("darkMode");
+                $("body").addClass("dark-mode");
+                $("#hero").addClass("hero-dark");
+                $("#divider-1").addClass("divider-bg-1-dark");
+                $("#divider-2").addClass("divider-bg-2-dark");
+                $(".card").addClass("card-dark");
+                $("#contact").addClass("contact-section-dark")
+                localStorage.setItem("darkMode", true);
+       }
+    }
