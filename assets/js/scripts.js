233b7f30911c08adfccd6ceee3334920fda507ed
Chris Birrell <vaiezmedia@gmail.com>
2020-10-26 13:51:11 +0000

    Misc : changed all instances of 'cart' to 'basket' & updated note for scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,13 +7,15 @@ $(function () {
         $('#map').toggleClass();
     });
 });
+
 // jQuery - toggle shopping cart modal when clicking cart icon
 
 $(function () {
-    $(".cart").on("click", function () {
-        $("#cartModal").modal("show");
+    $(".basket").on("click", function () {
+        $("#basketModal").modal("show");
     });
 });
+
 // jQuery - Allows user to select date and time for booking only on open days/hours and no previous date or time allowed
 
 $('#datetimepicker1').datetimepicker({
@@ -25,20 +27,17 @@ $('#datetimepicker1').datetimepicker({
     enabledHours: [16, 17, 18, 19, 20, 21, 22, 23]
 });
 
+//JQuery - Changes text on menu buttons when clicked for 5 seconds & greys out button
 
 (function () {
     $(".menu-btn").on("click", function () {
-
         var $this = $(this),
             oldText = $this.text();
-
         $this.text("Item added");
         $this.attr("disabled", "disabled");
-
         setTimeout(function () {
             $this.text(oldText);
             $this.removeAttr("disabled");
         }, 5000);
-
     });
 })();
