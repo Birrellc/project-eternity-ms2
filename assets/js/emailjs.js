126f76d689fa8120a98ae53393a3d023d9dc44b4
Chris Birrell <vaiezmedia@gmail.com>
2020-10-28 19:45:34 +0000

    FIX: Rollback to fix bug created with form submission

--- assets/js/emailjs.js
+++ assets/js/emailjs.js
@@ -1,33 +1,36 @@
-function sendMailBooking(bookingForm) {
-    emailjs.send("gmail", "eternity", {
-            "from_name": bookingForm.fullnamebooking.value,
-            "from_email": bookingForm.emailbooking.value,
-            "number_of_guests": bookingForm.tablesizeselect.value,
-            "date_time": bookingForm.date_time.value
-        })
-        .then(
-            function (success) {
-                alert("Form Submited Successfully!", success);
-            },
-            function (error) {
-                alert("Error detected please try again", error);
-            });
-    document.getElementById("bookingForm").reset();
+function sendMail(bookingForm) {
+    emailjs.send("gmail", "eternity",{
+        "from_name": bookingForm.fullnamebooking.value,
+        "from_email": bookingForm.emailbooking.value,
+        "number_of_guests": bookingForm.tablesizeselect.value,
+        "date_time": bookingForm.date_time.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
 }
 
+
 function sendMailContact(contactForm) {
-    emailjs.send("gmail", "eternity-contact", {
-            "from_name": contactForm.fullname.value,
-            "from_email": contactForm.email.value,
-            "subject": contactForm.subject.value,
-            "message": contactForm.message.value
-        })
-        .then(
-            function () {
-                alert("Form Submited Successfully!", success);
-            },
-            function (error) {
-                alert("Error detected please try again", error);
-            });
-    document.getElementById("#contactForm").reset();
+    emailjs.send("gmail", "eternity-contact",{
+        "from_name": contactForm.fullname.value,
+        "from_email": contactForm.email.value,
+        "subject": contactForm.subject.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
 }
