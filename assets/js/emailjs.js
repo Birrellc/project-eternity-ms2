ecca9efc2e9b7b64a60eb7be8cb4d9325fbe1f1b
Chris Birrell <vaiezmedia@gmail.com>
2020-10-29 12:54:42 +0000

    Feat: Removed bad practice of onSubmit function in forms - Replaced with eventListener Functions

--- assets/js/emailjs.js
+++ assets/js/emailjs.js
@@ -1,4 +1,4 @@
-function sendMail(contactForm) {
+/*function sendMail(contactForm) {
     emailjs.send("gmail", "eternity-contact", {
             "from_name": contactForm.fullname.value,
             "from_email": contactForm.email.value,
@@ -43,3 +43,53 @@ function sendMailBooking(bookingForm) {
             });
     return false;
 }
+*/
+let formContact = document.getElementById("contactForm");
+formContact.addEventListener("submit", function(e) {
+e.preventDefault();
+emailjs.send("gmail", "eternity-contact", {
+    "from_name": contactForm.fullNameContact.value,
+    "from_email": contactForm.emailContact.value,
+    "subject": contactForm.subjectContact.value,
+    "message": contactForm.messageContact.value
+})
+.then(
+    function () {
+        alert("success");
+        let submitButtonContact = document.getElementById("submitBtnContact");
+        submitButtonContact.innerHTML = "Submitted!";
+        document.getElementById("submitBtnContact").disabled = true;
+    },
+    function (error) {
+        alert("Error Please Try Again!")
+        console.log("FAILED", error);
+    });
+    document.getElementById('contactForm').reset();
+return false;
+
+});
+
+let formBooking = document.getElementById("bookingForm");
+formBooking.addEventListener("submit", function(e) {
+e.preventDefault();
+emailjs.send("gmail", "eternity", {
+    "from_name": bookingForm.fullNameBooking.value,
+    "from_email": bookingForm.emailBooking.value,
+    "number_of_guests": bookingForm.tableSizeSelectBooking.value,
+    "date_time": bookingForm.date_time.value
+})
+.then(
+    function () {
+        alert("success");
+        let submitButtonContact = document.getElementById("submitBtnBooking");
+        submitButtonContact.innerHTML = "Submitted!";
+        document.getElementById("submitBtnBooking").disabled = true;
+    },
+    function (error) {
+        alert("Error Please Try Again!")
+        console.log("FAILED", error);
+    });
+    document.getElementById('bookingForm').reset();
+return false;
+
+});
