35840c12d8f631e7bccc0911ebc08cb6e5ba21c3
Chris Birrell <vaiezmedia@gmail.com>
2020-11-09 13:14:22 +0000

    Misc: fixed spacing unintentional spacing issue in code

--- assets/js/color-scheme.js
+++ assets/js/color-scheme.js
@@ -1,4 +1,4 @@
-$(".dark-mode-switch").on('click', function() {
+$(".dark-mode-switch").on('click', function () {
     if ($("body").hasClass("dark-mode")) {
         $("body").removeClass("dark-mode");
         $("#home").removeClass("home-dark");
@@ -6,8 +6,8 @@ $(".dark-mode-switch").on('click', function() {
         $("#divider-2").removeClass("divider-bg-2-dark");
         $(".card").removeClass("card-dark");
         $("#contact").removeClass("contact-section-dark")
-        localStorage.removeItem("darkMode")}
-    else {
+        localStorage.removeItem("darkMode")
+    } else {
         $("body").addClass("dark-mode");
         $("#home").addClass("home-dark");
         $("#divider-1").addClass("divider-bg-1-dark");
@@ -18,15 +18,15 @@ $(".dark-mode-switch").on('click', function() {
     }
 });
 
-onload = function() {
-    if(localStorage.getItem("darkMode")) {
+onload = function () {
+    if (localStorage.getItem("darkMode")) {
         localStorage.getItem("darkMode");
-                $("body").addClass("dark-mode");
-                $("#home").addClass("home-dark");
-                $("#divider-1").addClass("divider-bg-1-dark");
-                $("#divider-2").addClass("divider-bg-2-dark");
-                $(".card").addClass("card-dark");
-                $("#contact").addClass("contact-section-dark")
-                localStorage.setItem("darkMode", true);
-       }
+        $("body").addClass("dark-mode");
+        $("#home").addClass("home-dark");
+        $("#divider-1").addClass("divider-bg-1-dark");
+        $("#divider-2").addClass("divider-bg-2-dark");
+        $(".card").addClass("card-dark");
+        $("#contact").addClass("contact-section-dark")
+        localStorage.setItem("darkMode", true);
     }
+}
